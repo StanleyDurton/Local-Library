250875942f8d0d22a945871b1cbbc05c13da7773
stanlu <2401177973@qq.com>
2019-12-07 22:09:10 +0800

    add to_database.py for loading data

--- to_database.py
+++ to_database.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
 import os
 import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibrarySystem.settings")
 django.setup()
 
-import requests
-from django.test import TestCase
-from bs4 import BeautifulSoup
-from lxml import etree
 import time
+import requests
+from bs4 import BeautifulSoup
 from datetime import datetime
+from django.test import TestCase
+from xlrd import open_workbook
 
 from locallibrary.models import Book
 
@@ -29,18 +28,18 @@ summary_list = []
 
 link_list = []
 
+# 链接数据库
+book_set = Book.objects.all()
+book_exist_list = []
+
+# 获取数据库中现有的图书名
+for item in book_set:
+    book_exist_list.append(item.book_name)
+print("数据库中已经存在的book：", book_exist_list)
+
 
 # 抓取豆瓣读书某个分类下的全部书籍列表信息
-def BookMethodTests(url):
-    # 链接数据库
-    book_set = Book.objects.all()
-    book_exist_list = []
-
-    # 获取数据库中现有的图书名
-    for item in book_set:
-        book_exist_list.append(item.book_name)
-    print("数据库中已经存在的book：", book_exist_list)
-
+def get_list_from_scraper(url):
     # 使用requests请求书籍页面列表
     # 加入header是为了增加浏览器请求代理，防止因请求被截断
     header = {
@@ -81,8 +80,6 @@ def BookMethodTests(url):
         title_string = soup.select("#subject_list > ul > li:nth-child(" + str(i) + ") > div.info > h2 > a")[0].string
         # print(str(title_string).strip())
         title_list.append(str(title_string).strip())
-        # subject_list > ul > li:nth-child(6) > div.info > h2 > a
-        # subject_list > ul > li:nth-child(3) > div.info > h2 > a
 
         # image src
         image_src = soup.select("#subject_list > ul > li:nth-child(" + str(i) + ") > div.pic > a > img")[0].get('src')
@@ -98,7 +95,7 @@ def BookMethodTests(url):
     # return link_list
 
 
-def getInfo(link_list):
+def get_summary_from_list(link_list):
     header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
     }
@@ -112,15 +109,15 @@ def getInfo(link_list):
         try:
             filter_set = soup.select('#link-report > div:nth-child(1) > div > p')[0].string
         except:
-            failed_list.append(link)
+            # failed_list.append(link)
             summary_list.append("None")
         else:
             summary_list.append(filter_set)
 
-    print(failed_list)
+    # print(failed_list)
 
 
-def getImage(image_list):
+def get_image_from_list(image_list):
     header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
     }
@@ -133,29 +130,19 @@ def getImage(image_list):
                 file.write(res.content)
 
 
-class DatabaseTest(TestCase):
-    # 书籍分类标签
-    tag = "society"
-    url = 'https://book.douban.com/tag/' + tag
+def load_data_from_list(url, tag):
 
-    # 链接数据库
-    book_set = Book.objects.all()
-    book_exist_list = []
+    # 爬取书籍内容
+    get_list_from_scraper(url)
+    get_summary_from_list(link_list)
+    get_image_from_list(image_list)
+    load_data_from_list(tag)
 
-    # 获取数据库中现有的图书名
-    for item in book_set:
-        book_exist_list.append(item.book_name)
-    print("数据库中已经存在的book：", book_exist_list)
-
-    BookMethodTests(url)
+    # 将数据记录保存到数据库中
+    cnt = 0
     for i in range(len(title_list)):
-        if title_list[i] != "None":
-            print(title_list[i], author_list[i], press_list[i], publish_date_list[i], price_list[i])
-    getInfo(link_list)
-
-    for i in range(len(title_list)):
-        if title_list[i] != "None" and summary_list != "None" and title_list[i] not in book_exist_list:
-            print(len(publish_date_list[i].strip()), publish_date_list[i])
+        if title_list[i] != "None" and title_list[i] not in book_exist_list:
+            # print(len(publish_date_list[i].strip()), publish_date_list[i])
             if len(publish_date_list[i].strip()) <= 7:
                 date = datetime.strptime(publish_date_list[i].strip(), "%Y-%m")
             else:
@@ -168,7 +155,54 @@ class DatabaseTest(TestCase):
                                              book_summary=summary_list[i],
                                              book_category_choice=tag)
             new_record.save()
-    getImage(image_list)
+            cnt += 1
+    print("Successfully execute: ", cnt)
+
+
+def load_data_from_file(file_path):
+    data = open_workbook(file_path)
+    table = data.sheets()[0]
+    row_number = table.nrows
+    cnt = 0
+    for i in range(1, row_number):
+        row = table.row_values(i)
+        if row[1] in book_exist_list:
+            continue
+        print(row)
+        # print(row[8])
+        if len(row[7].strip()) <= 7:
+            date = datetime.strptime(row[7].strip(), "%Y-%m")
+        else:
+            date = datetime.strptime(row[7].strip(), "%Y-%m-%d")
+
+        new_record = Book.objects.create(book_name=row[1],
+                                         book_author=row[2],
+                                         book_press=row[3],
+                                         book_isbn=row[4],
+                                         book_language=row[5],
+                                         book_price=row[6],
+                                         book_publish_date=date,
+                                         book_number=row[8],
+                                         book_category_choice=row[9],
+                                         book_summary=row[10],
+                                         )
+        new_record.save()
+        cnt += 1
+    print("successfully execute: ", cnt)
+
+
+class DatabaseTest(TestCase):
+    # 书籍分类标签
+    tag = "society"
+    url = 'https://book.douban.com/tag/' + tag
+    file_path = "C:/Users/imagi/Downloads/Book-2019-12-07.xls"
+
+    # 从爬虫中获取书籍，并写入数据库
+    # load_data_from_list(url, tag)
+
+    # 从文件中读取书籍，并写入数据库
+    load_data_from_file(file_path)
+
 
 
 
